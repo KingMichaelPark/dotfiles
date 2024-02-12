40196cb6d0f593e47be507a13c4e4c2d2c00cc7d
Michael Park <me@mhpark.me>
2024-02-12 13:35:54 +0000

    fix: neorg

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -4,18 +4,18 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
     config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/.notes",
-              },
+        require("neorg").setup {
+            load = {
+                ["core.defaults"] = {},  -- Loads default behaviour
+                ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.dirman"] = {      -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/Documents/neorg",
+                        },
+                    },
+                },
             },
-          },
-        },
-      }
+        }
     end
-  }
+}
