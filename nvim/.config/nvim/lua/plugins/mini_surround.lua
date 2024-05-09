f93f53448049c20520e8c7b8125473a28893b3e6
Michael <64011084+KingMichaelPark@users.noreply.github.com>
2024-05-09 08:56:52 +0100

    fix: mini surround mappings

--- nvim/.config/nvim/lua/plugins/mini_surround.lua
+++ nvim/.config/nvim/lua/plugins/mini_surround.lua
@@ -2,7 +2,21 @@ return {
     "echasnovski/mini.nvim",
     version = "*",
     event = "VeryLazy",
-    config = function()
-        require('mini.surround').setup()
-    end
+    opts = {
+        mappings = {
+          add = 'csa', -- Add surrounding in Normal and Visual modes
+          delete = 'csd', -- Delete surrounding
+          find = 'csf', -- Find surrounding (to the right)
+          find_left = 'csF', -- Find surrounding (to the left)
+          highlight = 'csh', -- Highlight surrounding
+          replace = 'csr', -- Replace surrounding
+          update_n_lines = 'csn', -- Update `n_lines`
+  
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      },
+      config = function(_, opts)
+          require('mini.surround').setup(opts)
+      end
 }
