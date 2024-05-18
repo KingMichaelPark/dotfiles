bd8494caf6b0c457533ba7c1f02b7c93645e6686
Michael Park <me@mhpark.me>
2024-05-18 17:48:05 +0100

    feat: Add colorhints

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -3,13 +3,14 @@ return {
     enabled = true,
     keys = {
         { "<leader>Cc", '<cmd>ChatGPT<cr>',                    desc = "ChatGPT", },
-        { "<leader>Ce", '<cmd>ChatGPTEditWithInstruction<cr>', { "n", "v" },     desc = "ChatGPT" },
-        { "<leader>Cd", "<cmd>ChatGPTRun docstring<CR>",       { "n", "v" },     desc = "Docstring" },
-        { "<leader>Ct", "<cmd>ChatGPTRun add_tests<CR>",       { "n", "v" },     desc = "Add Tests" },
-        { "<leader>Co", "<cmd>ChatGPTRun optimize_code<CR>",   { "n", "v" },     desc = "Optimize code" },
-        { "<leader>Cs", "<cmd>ChatGPTRun summarize<CR>",       { "n", "v" },     desc = "Optimize code" },
-        { "<leader>Cx", "<cmd>ChatGPTRun explain_code<CR>",    { "n", "v" },     desc = "Explain code" },
-        { "<leader>Cf", "<cmd>ChatGPTRun fix_bugs<CR>",        { "n", "v" },     desc = "Fix bugs" },
+        { "<leader>Ca", '<cmd>ChatGPTActAs<cr>',               desc = "ChatGPT Act As", },
+        { "<leader>Ce", '<cmd>ChatGPTEditWithInstruction<cr>', { "n", "v" },            desc = "ChatGPT" },
+        { "<leader>Cd", "<cmd>ChatGPTRun docstring<CR>",       { "n", "v" },            desc = "Docstring" },
+        { "<leader>Ct", "<cmd>ChatGPTRun add_tests<CR>",       { "n", "v" },            desc = "Add Tests" },
+        { "<leader>Co", "<cmd>ChatGPTRun optimize_code<CR>",   { "n", "v" },            desc = "Optimize code" },
+        { "<leader>Cs", "<cmd>ChatGPTRun summarize<CR>",       { "n", "v" },            desc = "Optimize code" },
+        { "<leader>Cx", "<cmd>ChatGPTRun explain_code<CR>",    { "n", "v" },            desc = "Explain code" },
+        { "<leader>Cf", "<cmd>ChatGPTRun fix_bugs<CR>",        { "n", "v" },            desc = "Fix bugs" },
     },
     config = function()
         local identity = vim.fn.expand('$HOME/.config/sops/age/keys.txt')
---
+++ nvim/.config/nvim/lua/plugins/highlight-colors.lua
@@ -0,0 +1,19 @@
+return {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+        require("nvim-highlight-colors").setup {
+            ---Render style
+            ---@usage 'background'|'foreground'|'virtual'
+            render = 'virtual',
+
+            ---Set virtual symbol (requires render to be set to 'virtual')
+            virtual_symbol = '■',
+
+            ---Highlight named colors, e.g. 'green'
+            enable_named_colors = true,
+
+            ---Highlight tailwind colors, e.g. 'bg-blue-500'
+            enable_tailwind = true,
+        }
+    end
+}
