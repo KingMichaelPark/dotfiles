1ea1093e6a4a0f0d572f9996f951843a38af116f
Michael Park <me@mhpark.me>
2024-06-17 20:50:57 +0100

    fix: mappings for tabs

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,12 +1,12 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { {
-        "nvim-lua/plenary.nvim", lazy=true}, {"nvim-telescope/telescope.nvim", lazy=true} },
+        "nvim-lua/plenary.nvim", lazy = true }, { "nvim-telescope/telescope.nvim", lazy = true } },
     config = true,
-    keys={
-        {"]t", function() require("todo-comments").jump_next() end,  desc = "Next todo comment" },
-        {"[t", function() require("todo-comments").jump_prev() end,  desc = "Previous todo comment" },
-        {"<leader>tq", "<cmd>TodoQuickFix<cr>",  desc = "Todo QuickFix" },
-        {"<leader>tt", "<cmd>TodoTelescope<cr>",  desc = "Todo Telescope" }
+    keys = {
+        { "]c",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+        { "[c",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+        { "<leader>cq", "<cmd>TodoQuickFix<cr>",                             desc = "Todo QuickFix" },
+        { "<leader>ct", "<cmd>TodoTelescope<cr>",                            desc = "Todo Telescope" }
     }
 }
