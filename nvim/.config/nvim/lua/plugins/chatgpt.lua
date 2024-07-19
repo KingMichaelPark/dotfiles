99e97bff5e611417f785b7bfdb12401c3b8a22ee
Michael Park <me@mhpark.me>
2024-07-19 11:57:15 +0100

    feat: switch gpt model to 4o-mini

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -4,13 +4,13 @@ return {
     keys = {
         { "<leader>Cc", '<cmd>ChatGPT<cr>',                    desc = "ChatGPT", },
         { "<leader>Ca", '<cmd>ChatGPTActAs<cr>',               desc = "ChatGPT Act As", },
-        { "<leader>Ce", '<cmd>ChatGPTEditWithInstruction<cr>', { "n", "v" },            desc = "ChatGPT" },
-        { "<leader>Cd", "<cmd>ChatGPTRun docstring<CR>",       { "n", "v" },            desc = "Docstring" },
-        { "<leader>Ct", "<cmd>ChatGPTRun add_tests<CR>",       { "n", "v" },            desc = "Add Tests" },
-        { "<leader>Co", "<cmd>ChatGPTRun optimize_code<CR>",   { "n", "v" },            desc = "Optimize code" },
-        { "<leader>Cs", "<cmd>ChatGPTRun summarize<CR>",       { "n", "v" },            desc = "Optimize code" },
-        { "<leader>Cx", "<cmd>ChatGPTRun explain_code<CR>",    { "n", "v" },            desc = "Explain code" },
-        { "<leader>Cf", "<cmd>ChatGPTRun fix_bugs<CR>",        { "n", "v" },            desc = "Fix bugs" },
+        { "<leader>Ce", '<cmd>ChatGPTEditWithInstruction<cr>', mode = { "v", "n" },     desc = "Edit with Instructions" },
+        { "<leader>Cd", "<cmd>ChatGPTRun docstring<CR>",       mode = "v",              desc = "Docstring" },
+        { "<leader>Ct", "<cmd>ChatGPTRun add_tests<CR>",       mode = "v",              desc = "Add Tests" },
+        { "<leader>Co", "<cmd>ChatGPTRun optimize_code<CR>",   mode = "v",              desc = "Optimize code" },
+        { "<leader>Cs", "<cmd>ChatGPTRun summarize<CR>",       mode = "v",              desc = "Optimize code" },
+        { "<leader>Cx", "<cmd>ChatGPTRun explain_code<CR>",    mode = "v",              desc = "Explain code" },
+        { "<leader>Cf", "<cmd>ChatGPTRun fix_bugs<CR>",        mode = "v",              desc = "Fix bugs" },
     },
     config = function()
         local identity = vim.fn.expand('$HOME/.config/sops/age/keys.txt')
@@ -24,7 +24,7 @@ return {
                     max_tokens = 4096
                 },
                 openai_edit_params = {
-                    model = "gpt-3.5-turbo",
+                    model = "gpt-4o-mini",
                     max_tokens = 4096
                 },
             })
