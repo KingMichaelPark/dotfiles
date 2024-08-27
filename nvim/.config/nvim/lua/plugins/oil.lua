adef453b5a4569e46b29b2d0ac31014ccc467580
Michael Park <me@mhpark.me>
2024-08-27 09:00:50 +0100

    fix: remove webdevicons from oil as I never use them

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,5 @@
 return {
     "stevearc/oil.nvim",
     keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } },
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = true
+    opts = { columns = {} },
 }
