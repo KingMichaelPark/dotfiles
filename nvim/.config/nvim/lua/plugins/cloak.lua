8fdfdfecc507a49e86292efaee1574a493d3972a
Michael Park <me@mhpark.me>
2024-11-03 13:36:37 +0000

    fix: cloak

--- nvim/.config/nvim/lua/plugins/cloak.lua
+++ nvim/.config/nvim/lua/plugins/cloak.lua
@@ -1,24 +1,20 @@
 return {
     "laytan/cloak.nvim",
     event = "VeryLazy",
-    enabled = false,
+    keys = {
+        { "<leader>st", "<cmd>:CloakToggle<cr>", desc = "[s]ecret [t]oggle" }
+    },
     config = function()
         require("cloak").setup({
             enabled = true,
             cloak_character = "*",
-            -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
             highlight_group = "Comment",
-            -- Applies the length of the replacement characters for all matched
-            -- patterns, defaults to the length of the matched pattern.
-            cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
-            -- Wether it should try every pattern to find the best fit or stop after the first.
+            cloak_length = nil,
             try_all_patterns = true,
             patterns = {
                 {
-                    -- Match any file starting with '.env'.
-                    -- This can be a table to match multiple file patterns.
-                    file_pattern = { ".env*", ".muse.toml", "^config$", "^credentials$" },
-                    cloak_pattern = { "=.+" },
+                    file_pattern = { ".env*", ".mise.toml", "^config$", "^credentials$" },
+                    cloak_pattern = '=.+',
                     replace = nil,
                 },
             },
