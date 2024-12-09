6e7f5e90aa16a9c6732fe2e59eaa283e35bc5d78
Michael Park <me@mhpark.me>
2024-12-09 16:49:08 +0000

    feat: update lazy mapping

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -12,7 +12,7 @@ return {
                     { icon = "󱪞", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
                     { icon = "󱔗", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
                     { icon = "", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-                    { icon = "󰒲", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    { icon = "󰒲", key = "p", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
                     { icon = "", key = "m", desc = "Mason", action = ":Mason" },
                     { icon = "󰈆", key = "q", desc = "Quit", action = ":qa" },
                 },
