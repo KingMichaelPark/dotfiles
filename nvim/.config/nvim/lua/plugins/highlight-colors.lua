f512dcd88a2593d36421b5c7194b29d1c2fc18bc
Michael Park <me@mhpark.me>
2025-02-18 19:51:08 +0000

    feat: update formatting for highlight-colors

--- nvim/.config/nvim/lua/plugins/highlight-colors.lua
+++ nvim/.config/nvim/lua/plugins/highlight-colors.lua
@@ -9,7 +9,7 @@ return {
                 render = 'virtual',
 
                 ---Set virtual symbol (requires render to be set to 'virtual')
-                virtual_symbol = '■',
+                virtual_symbol = '⬤',
 
                 ---Set virtual symbol suffix (defaults to '')
                 virtual_symbol_prefix = '',
@@ -22,7 +22,7 @@ return {
                 ---inline mimics VS Code style
                 ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
                 ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
-                virtual_symbol_position = 'inline',
+                virtual_symbol_position = 'eol',
 
                 ---Highlight hex colors, e.g. '#FFFFFF'
                 enable_hex = true,
@@ -43,7 +43,7 @@ return {
                 enable_named_colors = false,
 
                 ---Highlight tailwind colors, e.g. 'bg-blue-500'
-                enable_tailwind = false,
+                enable_tailwind = true,
             }
         })
     end,
