9ae5de2f14a15205c9b3cd539e22c1cd130b6520
Michael Park <me@mhpark.me>
2025-05-09 13:19:49 +0100

    feat: simplify mason and lsp configuration

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,19 @@
 return {
     "neovim/nvim-lspconfig",
+    dependencies = {
+        { "KingMichaelPark/mason.nvim" },
+        { "mason-org/mason-lspconfig.nvim" }
+    },
+    priority = 1000,
+    build = ":MasonUpdate",
+    config = function()
+        require("mason").setup({
+            pip = {
+                use_uv = true,
+            },
+        })
+        require("mason-lspconfig").setup {
+            ensure_installed = { "basedpyright", "ruff", "lua_ls" },
+        }
+    end
 }
