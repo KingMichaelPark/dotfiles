06d35a6fbd85e6f4cb18028a637385ca5c11c2b8
Michael Park <me@mhpark.me>
2025-07-03 09:13:01 +0100

    feat: suggested tweaks for leap

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,5 +1,20 @@
 return {
     "ggandor/leap.nvim",
     event = "BufReadPre",
-    config = function() require("leap").add_default_mappings() end,
+    config = function()
+        require("leap").add_default_mappings()
+        -- Exclude whitespace and the middle of alphabetic words from preview:
+        --   foobar[baaz] = quux
+        --   ^----^^^--^^-^-^--^
+        require('leap').opts.preview_filter =
+            function(ch0, ch1, ch2)
+                return not (
+                    ch1:match('%s') or
+                    ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
+                )
+            end
+
+        require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+        require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+    end,
 }
